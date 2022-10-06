6545be65528463dcce97684a95759d0aee8be416
Chivaughan <chivaughan007@gmail.com>
2022-10-06 14:35:10 +0100

    Added Saturday and Sunday to Weekdays.ts

--- src/Weekdays.ts
+++ src/Weekdays.ts
@@ -1,3 +1,4 @@
-type WeekDay = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
+type WeekDay = "Monday" | "Tuesday" | "Wednesday"
+    | "Thursday" | "Friday" | "Saturday" | "Sunday";
 
 let today: WeekDay = "Thursday";
